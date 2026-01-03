96e305a7d3468a968c4f326f7b2326f2a20c6912
ondriasma <ondriasma@protonmail.com>
2026-01-03 21:10:53 +0100

    Added command line argument

--- main.c
+++ main.c
@@ -8,26 +8,40 @@
 
 
 int main(int argc, char *argv[]) {
-    // todo - umožnit zadat jméno souboru s FS jako parametr
-    const char *test_filename = "testfs.img";
-    filesystem_t fs = {0};
-    fs.filename = test_filename;
-    
-    fs.file = fopen(test_filename, "r+b");
-    if (!fs.file) fs.file = fopen(test_filename, "w+b");
-    
-    if (!fs.file) {
-        fprintf(stderr, "Cannot open test filesystem file\n");
-        return 1;
+
+    char *filename = "filesystem";
+    if (argc > 1) {
+        filename = argv[1];
     }
+
+    filesystem_t fs = {0};
+    fs.filename = filename;
+    //pokus o otevření nebo nytvoření souboru
+    fs.file = fopen(filename, "r+b");
+    if (!fs.file) {
+        fs.file = fopen(filename, "w+b");
+        if (!fs.file) {
+            fprintf(stderr, "Soubor nelze vytvořit '%s'\n", filename);
+            return 1;
+        }
+        printf("Vytvořen nový filesystém '%s'\n", filename);
+        printf("Použijte příkaz 'format <size>' pro jeho naformátování.\n");
+    }
+
+    bool is_formatted = false;
     
     // načtení existujícího fs
     fseek(fs.file, 0, SEEK_END);
-    if (ftell(fs.file) >= (long)sizeof(superblock_t)) {
+    if (ftell(fs.file) >= sizeof(superblock_t)) {   //soubor menší než superblock nemůže být validní fs
         fseek(fs.file, 0, SEEK_SET);
-        if (load_superblock(&fs) && strcmp(fs.sb.signature, SIGNATURE) == 0) {
+        if (load_superblock(&fs)) {
             load_bitmaps(&fs);
             strcpy(fs.current_path, "/");
+            is_formatted = true;
+            printf("Načítám filesystem\n");
+        } else {
+            printf("Soubor není validní filesystém.\n");
+            printf("Použijte příkaz 'format <size>' pro jeho naformátování.\n");
         }
     }
     
@@ -41,7 +55,15 @@ int main(int argc, char *argv[]) {
         sscanf(line, "%s %s %s", cmd, arg1, arg2);
         
         if (strcmp(cmd, "exit") == 0) break;
-        else if (strcmp(cmd, "format") == 0) format(&fs, arg1);
+        else if (strcmp(cmd, "format") == 0) {
+            if (format(&fs, arg1)) {
+                is_formatted = true;
+            }
+        }
+        else if (!is_formatted) {
+            printf("Filesystém není naformátovaný. Použijte příkaz 'format <size>'\n");
+            continue;
+        }
         else if (strcmp(cmd, "mkdir") == 0) mkdir(&fs, arg1);
         else if (strcmp(cmd, "pwd") == 0) pwd(&fs);
         else if (strcmp(cmd, "ls") == 0) ls(&fs, arg1[0] ? arg1 : NULL);
@@ -56,7 +78,7 @@ int main(int argc, char *argv[]) {
         else if (strcmp(cmd, "mv") == 0) mv(&fs, arg1, arg2);
         else if (strcmp(cmd, "outcp") == 0) outcp(&fs, arg1, arg2);
         else if (strcmp(cmd, "load") == 0) load(&fs, arg1);
-        else printf("Unknown command\n");
+        else printf("Neznámý příkaz\n");
     }
     
     if (fs.inode_bitmap) free(fs.inode_bitmap);
